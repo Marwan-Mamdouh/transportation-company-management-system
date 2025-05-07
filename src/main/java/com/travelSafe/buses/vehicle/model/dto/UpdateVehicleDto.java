6412bac1cd24aec6b1f4fff1c1f08f5b4c17fc05
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-07 21:02:04 +0300

    change file name to be more meaningful

--- src/main/java/com/travelSafe/buses/vehicle/model/dto/UpdateVehicleDto.java
+++ src/main/java/com/travelSafe/buses/vehicle/model/dto/UpdateVehicleDto.java
@@ -1,5 +1,16 @@
-package com.travelSafe.buses.vehicle.model;
+package com.travelSafe.buses.vehicle.model.dto;
 
-public record UpdateVehicleCommand(Integer vehicleId, Vehicle vehicle) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
+public record UpdateVehicleDto(@Max(255) Integer vehicleId,
+                               @NotBlank(message = "Plate number can not be null or empty") @Size(max = 10, message = "plate number must be 10 character max.") String plateNumber,
+                               @Positive @Max(value = 60, message = "What is this vehicle that can hold more then 60 person ?") Integer capacity,
+                               @NotBlank(message = "vehicle level can not be null or blank.") @Size(max = 10) String vehicleLevel,
+                               @NotBlank LocalDate licenseIssueDate,
+                               @NotBlank LocalDate licenseExpiryDate) {
 
 }
