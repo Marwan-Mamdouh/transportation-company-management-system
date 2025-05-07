fdc5a585dc493aa6fe458c2d190236f4cce96218
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-07 21:17:16 +0300

    add a new exception class to handle duplicate vehicle plate number

--- src/main/java/com/travelSafe/buses/exceptions/vehicle/DuplicateVehiclePlateNumberException.java
+++ src/main/java/com/travelSafe/buses/exceptions/vehicle/DuplicateVehiclePlateNumberException.java
@@ -1,7 +1,19 @@
 package com.travelSafe.buses.exceptions.vehicle;
 
+import com.travelSafe.buses.exceptions.ErrorMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateVehiclePlateNumberException extends RuntimeException {
-  public DuplicateVehiclePlateNumberException(String message) {
-    super(message);
+
+  private static final Logger logger = LoggerFactory.getLogger(
+      DuplicateVehiclePlateNumberException.class);
+
+  public DuplicateVehiclePlateNumberException() {
+    super(ErrorMessage.DUPLICATE_VEHICLE_PLATE_NUMBER.getMessage());
+    logger.error("Exception: {}thrown.", getClass());
   }
 }
