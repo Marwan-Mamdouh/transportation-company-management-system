1c7c78723decb65136ba58665d5fac347baa2b97
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-12 04:22:18 +0300

    add validations

--- src/main/java/com/travelSafe/buses/department/model/DTO/PromoteManagerDTO.java
+++ src/main/java/com/travelSafe/buses/department/model/DTO/PromoteManagerDTO.java
@@ -1,12 +1,11 @@
 package com.travelSafe.buses.department.model.DTO;
 
-import com.travelSafe.buses.department.model.Department;
 import com.travelSafe.buses.validator.PatternLong;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record PromoteManagerDTO(
-    @Max(39999999999999L) @PatternLong(regexp = "^[2-3][0-9]{13}$") Long managerId,
-    @NotNull Department department) {
+    @NotNull @Positive @PatternLong(regexp = "^[2-3][0-9]{13}$") Long managerId,
+    @NotNull @Positive Integer departmentId) {
 
 }
