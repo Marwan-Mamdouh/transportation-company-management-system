1aad4c5e1b6a08898a843d13fa11e17e55994707
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-12 21:55:55 +0300

    minor changes

--- src/main/java/com/travelSafe/buses/vehicle/model/dto/VehicleResponseDTO.java
+++ src/main/java/com/travelSafe/buses/vehicle/model/dto/VehicleResponseDTO.java
@@ -4,12 +4,12 @@ import com.travelSafe.buses.vehicle.model.Vehicle;
 import lombok.Data;
 
 @Data
-public class ResponseVehicleDTO {
+public class VehicleResponseDTO {
 
   private final Integer vehicleId, capacity;
   private final String vehicleLevel, plateNumber;
 
-  public ResponseVehicleDTO(Vehicle vehicle) {
+  public VehicleResponseDTO(Vehicle vehicle) {
     this.vehicleId = vehicle.getVehicleId();
     this.capacity = vehicle.getCapacity();
     this.vehicleLevel = vehicle.getVehicleLevel();
