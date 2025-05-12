1d494baddf08ad9527820d0280abea35e9f92a43
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-12 21:51:06 +0300

    clean up entity class and remove the validations

--- src/main/java/com/travelSafe/buses/trip/model/Trip.java
+++ src/main/java/com/travelSafe/buses/trip/model/Trip.java
@@ -14,9 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -44,10 +42,7 @@ public class Trip {
   @JoinColumn(name = "driver_ssn", nullable = false)
   @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
   private Employee driver;
-
-  @NotNull
-  @Positive
-  @Max(10000)
+  
   @Column(name = "trip_price", nullable = false)
   private Integer price;
 
