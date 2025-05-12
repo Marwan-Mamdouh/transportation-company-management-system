bcca803d2cb79f3c310f46ff9d46f18f4964e501
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-12 03:03:25 +0300

    add method to handle the convetions between dto to entity

--- src/main/java/com/travelSafe/buses/employee/model/EmployeeMapper.java
+++ src/main/java/com/travelSafe/buses/employee/model/EmployeeMapper.java
@@ -1,8 +1,12 @@
 package com.travelSafe.buses.employee.model;
 
+import com.travelSafe.buses.employee.model.DTO.InputEmployeeDTO;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 //  Employee ToDTO(CreateEmployeeDTO employee);
+
+
+  Employee employeeFromInputDTO(InputEmployeeDTO dto);
 }
