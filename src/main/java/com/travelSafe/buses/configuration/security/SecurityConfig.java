59a7d8ea8ce5ac40a51e968329616b68916ce225
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-15 02:21:34 +0300

    remove some un wanted files to create new authentication method with jwt

--- src/main/java/com/travelSafe/buses/configuration/security/SecurityConfig.java
+++ src/main/java/com/travelSafe/buses/configuration/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,11 +16,6 @@ public class SecurityConfiguration {
   @Bean
   public AuthenticationManager authenticationManager(HttpSecurity request) throws Exception {
     return request.getSharedObject(AuthenticationManagerBuilder.class).build();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
   }
 
   @Bean
