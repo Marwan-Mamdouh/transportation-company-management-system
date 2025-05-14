761472ef314caa348ce98d030c2168c1f3c83283
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-14 04:54:49 +0300

    add password and validation to it

--- src/main/java/com/travelSafe/buses/domain/employee/model/DTO/InputEmployeeDTO.java
+++ src/main/java/com/travelSafe/buses/domain/employee/model/DTO/InputEmployeeDTO.java
@@ -1,4 +1,4 @@
-package com.travelSafe.buses.domin.employee.model.DTO;
+package com.travelSafe.buses.domain.employee.model.DTO;
 
 import com.travelSafe.buses.validator.PatternLong;
 import jakarta.validation.constraints.Email;
@@ -13,7 +13,7 @@ public record InputEmployeeDTO(
     @NotNull(message = "ssn can't be null.") @PatternLong(regexp = "^[2-3][0-9]{13}$") Long ssn,
     @NotBlank(message = "First name can't be null or empty.") @Size(max = 55, message = "First name length should be 55 character max.") String firstname,
     @NotBlank(message = "Last name can't be null or empty.") @Size(max = 55, message = "Last name length should be 55 character max.") String lastname,
-    @NotBlank(message = "Email can't be null or empty.") @Email(message = "Not valid Email.") @Size(max = 99, message = "Email length should be 99 character max.") String email,
+    @NotBlank(message = "Email can't be null or empty.") @Size(max = 99, message = "Email length should be 99 character max.") @Email String email,
     @NotBlank(message = "Password can't be null or empty.") @Size(min = 8, max = 20, message = "Password length should be between 8 and 20 character.") String password,
     @NotBlank(message = "Phone number can't be null or empty.") @Size(min = 11, max = 11, message = "Phone number length should be 11 character.") @Pattern(regexp = "^01[0-2,5][0-9]{8}$", message = "Not Valid Egyptian phone number.") String phoneNumber,
     @NotNull(message = "Birthday can't be null.") @Past(message = "Birthday can't be in the future.") LocalDate birthday) {
