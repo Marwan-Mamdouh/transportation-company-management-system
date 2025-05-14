f63a23c68819dbdf7fadae8bff3c728cc6f63edd
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-14 04:50:53 +0300

    comment two end points and start work of two

--- src/main/java/com/travelSafe/buses/domain/employee/controller/EmployeeAuthenticationController.java
+++ src/main/java/com/travelSafe/buses/domain/employee/controller/EmployeeAuthenticationController.java
@@ -1,6 +1,9 @@
 package com.travelSafe.buses.domain.employee.controller;
 
 import com.travelSafe.buses.domain.employee.model.DTO.EmployeeLoginDTO;
+import com.travelSafe.buses.domain.employee.model.DTO.EmployeeResponseDTO;
+import com.travelSafe.buses.domain.employee.model.DTO.InputEmployeeDTO;
+import com.travelSafe.buses.domain.employee.services.CreateEmployeeService;
 import com.travelSafe.buses.domain.employee.services.EmployeeLoginService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-public class EmployeeLoginController {
+public class EmployeeAuthenticationController {
 
   private final EmployeeLoginService employeeLoginService;
-//  private final CreateEmployeeService createEmployeeService;
+  private final CreateEmployeeService createEmployeeService;
 
-  public EmployeeLoginController(EmployeeLoginService employeeLoginService
-//      , CreateEmployeeService createEmployeeService
-  ) {
+  public EmployeeAuthenticationController(EmployeeLoginService employeeLoginService,
+      CreateEmployeeService createEmployeeService) {
     this.employeeLoginService = employeeLoginService;
-//    this.createEmployeeService = createEmployeeService;
+    this.createEmployeeService = createEmployeeService;
   }
 
-//  @PostMapping("/register")
-//  public ResponseEntity<String> createNewEmployee(
-//      @Valid @RequestBody InputEmployeeDTO empLoginCre) {
-//    return ResponseEntity.ok(createEmployeeService.execute(empLoginCre));
-//  }
+  @PostMapping("/register")
+  public ResponseEntity<EmployeeResponseDTO> createNewEmployee(
+      @Valid @RequestBody InputEmployeeDTO empLoginCre) {
+    return ResponseEntity.ok(new EmployeeResponseDTO(createEmployeeService.execute(empLoginCre)));
+  }
 
   @PostMapping("/login")
   public ResponseEntity<String> login(@Valid @RequestBody EmployeeLoginDTO empLoginCre) {
     return ResponseEntity.ok(employeeLoginService.execute(empLoginCre));
   }
 
-  @PostMapping("/refresh")
-  public ResponseEntity<String> refresh(@RequestBody EmployeeLoginDTO empLoginCre) {
-    return null;
-  }
-
-  @PostMapping("/logout")
-  public ResponseEntity<String> logout(@RequestBody EmployeeLoginDTO empLoginCre) {
-    return null;
-  }
+//  @PostMapping("/refresh")
+//  public ResponseEntity<String> refresh(@RequestBody EmployeeLoginDTO empLoginCre) {
+//    return null;
+//  }
+//
+//  @PostMapping("/logout")
+//  public ResponseEntity<String> logout(@RequestBody EmployeeLoginDTO empLoginCre) {
+//    return null;
+//  }
 }
