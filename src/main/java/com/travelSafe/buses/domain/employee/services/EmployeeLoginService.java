cd65eea5cb6467263954da97a9a369227c49cdcd
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-05-15 03:24:45 +0300

    use custom login method and call the new way to get tokens

--- src/main/java/com/travelSafe/buses/domain/employee/services/EmployeeLoginService.java
+++ src/main/java/com/travelSafe/buses/domain/employee/services/EmployeeLoginService.java
@@ -1,43 +1,46 @@
 package com.travelSafe.buses.domain.employee.services;
 
 import com.travelSafe.buses.Query;
-import com.travelSafe.buses.domain.employee.model.DTO.EmployeeLoginDTO;
-import com.travelSafe.buses.configuration.security.jwt.JwtUtil;
+import com.travelSafe.buses.domain.employee.EmployeeRepository;
+import com.travelSafe.buses.domain.employee.model.dto.EmployeeLoginDTO;
+import com.travelSafe.buses.exceptions.employee.EmailNotFoundException;
+import com.travelSafe.buses.exceptions.employee.NotValidPasswordException;
+import com.travelSafe.buses.util.JwtActions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeLoginService implements Query<EmployeeLoginDTO, String> {
 
-  private final AuthenticationManager manager;
+  private final JwtActions jwtActions;
+  private final EmployeeRepository employeeRepository;
+  private final BCryptPasswordEncoder passwordEncoder;
   private static final Logger logger = LoggerFactory.getLogger(EmployeeLoginService.class);
 
-  public EmployeeLoginService(AuthenticationManager manager) {
-    this.manager = manager;
+  public EmployeeLoginService(BCryptPasswordEncoder passwordEncoder,
+      EmployeeRepository employeeRepository, JwtActions jwtActions) {
+    this.passwordEncoder = passwordEncoder;
+    this.employeeRepository = employeeRepository;
+    this.jwtActions = jwtActions;
   }
 
   @Override
   public String execute(EmployeeLoginDTO input) {
     logger.info("Executing: {} with input: {}", getClass(), input);
-    // Create an authentication token using the provided username and password
-    // and authenticate it using the AuthenticationManager.
-    // This will throw an exception if the authentication fails.
-    final var token = new UsernamePasswordAuthenticationToken(input.email(), input.password());
-    logger.info("making user token: {} with input: {}", token, input);
+    final var employee = employeeRepository.findByEmail(input.email())
+        .orElseThrow(EmailNotFoundException::new);
 
-    // If authentication is successful, you can proceed with generating a JWT token
-    // or any other logic you need to perform after successful login.
-    logger.info("before auth with input: {}", token);
-    final Authentication auth = manager.authenticate(token);
-    logger.info("before auth context holder: {} with input: {}", auth, token);
-    SecurityContextHolder.getContext().setAuthentication(auth);
-    logger.info("after auth: {} with input: {}", auth, token);
-    return JwtUtil.generateToken((User) auth.getPrincipal());
+    if (!verifyPassword(input.password(), employee.getPassword())) {
+      logger.error("Invalid password for email: {}", input.email());
+      throw new NotValidPasswordException();
+    }
+    return jwtActions.jwtCreate(employee.getEmail(), employee.getRole());
+  }
+
+  private boolean verifyPassword(String password, String passwordHash) {
+    logger.info("Verifying password:");
+    return passwordEncoder.matches(password, passwordHash);
   }
 }
