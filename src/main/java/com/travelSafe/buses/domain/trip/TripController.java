b244be60be1e94cd80c5dcc7fcea6f9fc74de943
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-22 02:12:35 +0300

    refactor: remove versions from end points

--- src/main/java/com/travelSafe/buses/domain/trip/TripController.java
+++ src/main/java/com/travelSafe/buses/domain/trip/TripController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/trip")
+@RequestMapping("/api/trip")
 public class TripController {
 
   private final CreateTripService createTripService;
