6f0efee07859175edca6d2b5e0c96ab6362421ae
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-22 23:16:03 +0300

    fix(employee mapper): fix mapper methods and make all conversions in mapper interface

--- src/main/java/com/travel/safe/buses/domain/employee/EmployeeMapper.java
+++ src/main/java/com/travel/safe/buses/domain/employee/EmployeeMapper.java
@@ -1,13 +1,23 @@
 package com.travel.safe.buses.domain.employee;
 
-import com.travel.safe.buses.domain.employee.dto.InputEmployeeDTO;
+import com.travel.safe.buses.domain.employee.dto.CreateEmployeeDTO;
+import com.travel.safe.buses.domain.employee.dto.EmployeePaycheckDTO;
+import com.travel.safe.buses.domain.employee.dto.EmployeeResponseDTO;
 import com.travel.safe.buses.domain.employee.model.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
-//  Employee ToDTO(CreateEmployeeDTO employee);
 
+  EmployeeResponseDTO responseDTOFromEmployee(Employee employee);
 
-  Employee employeeFromInputDTO(InputEmployeeDTO dto);
+  @Mapping(target = "salary", source = "department.salary")
+  EmployeePaycheckDTO payCheckDTOFromEmployee(Employee employee);
+
+  @Mapping(target = "age", ignore = true)
+  @Mapping(target = "role", ignore = true)
+  @Mapping(target = "department", ignore = true)
+  @Mapping(target = "supervisor", ignore = true)
+  Employee employeeCreateEmployeeDTO(CreateEmployeeDTO dto);
 }
