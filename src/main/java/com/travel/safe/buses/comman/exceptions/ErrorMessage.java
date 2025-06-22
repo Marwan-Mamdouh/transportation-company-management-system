1a16a9bd353ee2c1e4c02e52b8f7808c5597ff81
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-22 20:20:33 +0300

    fix(error messages): fix some typos in some error messages

--- src/main/java/com/travel/safe/buses/comman/exceptions/ErrorMessage.java
+++ src/main/java/com/travel/safe/buses/comman/exceptions/ErrorMessage.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessage {
   EMPLOYEE_NOT_FOUND("Employee not found."), DUPLICATE_EMPLOYEE_ID(
-      "Duplicate employee Id."), EMAIL_NOT_FOUND("Email not valid"), PASSWORD_NOT_VALID(
+      "Duplicate employee Id."), NOT_VALID_CREDENTIALS(
+      "Credentials are not valid."), DUPLICATE_EMPLOYEE_DATA(
+      "Duplicate Employee data, check SSN, email or phone number."), PASSWORD_NOT_VALID(
       "Password Not valid."), DUPLICATE_EMAIL("Duplicate email."), DUPLICATE_PHONE_NUMBER(
       "Duplicate phone number."), SUPERVISOR_NOT_FOUND(
       "Supervisor not found."), DEPARTMENT_NOT_FOUND(
@@ -13,7 +15,7 @@ public enum ErrorMessage {
       "Duplicate department name."), TRAVEL_LINE_NOT_FOUND(
       "Travel line not found."), TRAVEL_LINE_NOT_VALID("Travel line not valid."), TRIP_NOT_FOUND(
       "Trip not found."), TRIP_NOT_VALID("Trip not valid."), SEAT_NOT_FOUND(
-      "Seat not found."), SEAT_ALREADY_BOOKED(
+      "No Available Seats founded."), SEAT_ALREADY_BOOKED(
       "seat already booked by other customer."), VEHICLE_NOT_FOUND(
       "Vehicle not found."), DUPLICATE_VEHICLE_ID(
       "duplicate vehicle Id."), DUPLICATE_VEHICLE_PLATE_NUMBER("Duplicate vehicle plate number.");
