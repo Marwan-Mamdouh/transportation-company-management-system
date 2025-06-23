e9838d2a96144bf35121d4ea1f461ed3f545a9ca
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-23 20:28:56 +0300

    refactor: change class name and use specifications with it

--- src/main/java/com/travel/safe/buses/domain/employee/services/get/GetEmployeesBy.java
+++ src/main/java/com/travel/safe/buses/domain/employee/services/get/GetEmployeesBy.java
@@ -1,7 +1,12 @@
 package com.travel.safe.buses.domain.employee.services.get;
 
+import static com.travel.safe.buses.domain.employee.EmployeeSpecification.hasDepartment;
+import static com.travel.safe.buses.domain.employee.EmployeeSpecification.hasSupervisor;
+import static org.springframework.data.jpa.domain.Specification.where;
+
 import com.travel.safe.buses.comman.shared.Query;
 import com.travel.safe.buses.domain.employee.EmployeeRepository;
+import com.travel.safe.buses.domain.employee.dto.EmployeeSpecificationDTO;
 import com.travel.safe.buses.domain.employee.model.Employee;
 import java.util.List;
 import org.slf4j.Logger;
@@ -9,19 +14,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GetEmployeesBySupervisorService implements Query<Long, List<Employee>> {
+public class GetEmployeesBy implements Query<EmployeeSpecificationDTO, List<Employee>> {
 
-  private static final Logger logger = LoggerFactory.getLogger(
-      GetEmployeesBySupervisorService.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(GetEmployeesBy.class);
   private final EmployeeRepository employeeRepository;
 
-  public GetEmployeesBySupervisorService(EmployeeRepository employeeRepository) {
+  public GetEmployeesBy(EmployeeRepository employeeRepository) {
     this.employeeRepository = employeeRepository;
   }
 
   @Override
-  public List<Employee> execute(Long input) {
-    logger.debug("Executing: {} with input: {}", getClass(), input);
-    return employeeRepository.findBySupervisor_EmployeeId(input);
+  public List<Employee> execute(EmployeeSpecificationDTO input) {
+    LOGGER.debug("Executing: {} with input: {}", getClass(), input);
+    return employeeRepository.findAll(
+        where(hasDepartment(input.departmentId()).and(hasSupervisor(input.supervisorId()))));
   }
 }
