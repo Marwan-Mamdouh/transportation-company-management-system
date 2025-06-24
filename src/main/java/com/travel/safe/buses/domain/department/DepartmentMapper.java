2935b79b48ce3ed1370e7a982da06cbbc7ae0561
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-25 00:02:27 +0300

    fix(WARN): ignore some properities in the mapper

--- src/main/java/com/travel/safe/buses/domain/department/DepartmentMapper.java
+++ src/main/java/com/travel/safe/buses/domain/department/DepartmentMapper.java
@@ -5,15 +5,21 @@ import com.travel.safe.buses.domain.department.DTO.DepartmentResponseDTO;
 import com.travel.safe.buses.domain.department.DTO.UpdatedDepartmentDTO;
 import com.travel.safe.buses.domain.department.model.Department;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
 
-//  DepartmentMapper INSTANCE = Mappers.getMapper(DepartmentMapper.class);
-
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "departmentManager", ignore = true)
   Department toEntity(CreateDepartmentDto department);
 
+  @Mapping(target = "departmentManager", ignore = true)
   Department toEntity(UpdatedDepartmentDTO department);
 
-  DepartmentResponseDTO toBaseDto(Department department);
+  @Mapping(target = "salary", ignore = true)
+  @Mapping(target = "departmentManager", ignore = true)
+  Department responseDtoToEntity(DepartmentResponseDTO responseDTO);
+
+  DepartmentResponseDTO entityToDto(Department department);
 }
