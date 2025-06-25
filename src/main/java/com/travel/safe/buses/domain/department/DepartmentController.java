c251269105d4a6f2a4418923f5b7b0924950c8e7
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-26 02:57:36 +0300

    refactor: now end point method just return what ever the service layer does

--- src/main/java/com/travel/safe/buses/domain/department/DepartmentController.java
+++ src/main/java/com/travel/safe/buses/domain/department/DepartmentController.java
@@ -1,9 +1,10 @@
 package com.travel.safe.buses.domain.department;
 
-import com.travel.safe.buses.domain.department.DTO.CreateDepartmentDto;
-import com.travel.safe.buses.domain.department.DTO.DepartmentResponseDTO;
-import com.travel.safe.buses.domain.department.DTO.PromoteManagerDTO;
-import com.travel.safe.buses.domain.department.DTO.UpdatedDepartmentDTO;
+import com.travel.safe.buses.domain.department.dto.CreateDepartmentDto;
+import com.travel.safe.buses.domain.department.dto.DepartmentResponseDTO;
+import com.travel.safe.buses.domain.department.dto.DepartmentSearchDTO;
+import com.travel.safe.buses.domain.department.dto.PromoteManagerDTO;
+import com.travel.safe.buses.domain.department.dto.UpdatedDepartmentDTO;
 import com.travel.safe.buses.domain.department.model.Department;
 import com.travel.safe.buses.domain.department.service.CreateDepartmentService;
 import com.travel.safe.buses.domain.department.service.DeleteDepartmentService;
@@ -13,7 +14,7 @@ import com.travel.safe.buses.domain.department.service.PromoteManagerService;
 import com.travel.safe.buses.domain.department.service.UpdateDepartmentService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
-import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,9 +40,8 @@ public class DepartmentController {
   private final PromoteManagerService promoteManagerService;
 
   public DepartmentController(CreateDepartmentService createDepartmentService,
-      DeleteDepartmentService deleteDepartmentService,
-      GetDepartmentService getDepartmentService, GetDepartmentsService getDepartmentsService,
-      UpdateDepartmentService updateDepartmentService,
+      DeleteDepartmentService deleteDepartmentService, GetDepartmentService getDepartmentService,
+      GetDepartmentsService getDepartmentsService, UpdateDepartmentService updateDepartmentService,
       PromoteManagerService promoteManagerService) {
     this.createDepartmentService = createDepartmentService;
     this.deleteDepartmentService = deleteDepartmentService;
@@ -54,34 +54,31 @@ public class DepartmentController {
   @PostMapping
   public ResponseEntity<DepartmentResponseDTO> createDepartment(
       @Valid @RequestBody CreateDepartmentDto department) {
-    final Department savedDepartment = createDepartmentService.execute(department);
-    return ResponseEntity.ok(new DepartmentResponseDTO(savedDepartment));
+    return ResponseEntity.ok(createDepartmentService.execute(department));
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<DepartmentResponseDTO> getDepartment(@PathVariable @Positive Integer id) {
-    final Department department = getDepartmentService.execute(id);
-    return ResponseEntity.ok(new DepartmentResponseDTO(department));
+    return ResponseEntity.ok(getDepartmentService.execute(id));
   }
 
   @GetMapping
-  public ResponseEntity<List<DepartmentResponseDTO>> getDepartments() {
-    final List<Department> department = getDepartmentsService.execute(null);
-    return ResponseEntity.ok(department.stream().map(DepartmentResponseDTO::new).toList());
+  public ResponseEntity<Page<Department>> getDepartments(
+      @RequestBody DepartmentSearchDTO departmentSearchDTO) {
+    final Page<Department> departments = getDepartmentsService.execute(departmentSearchDTO);
+    return ResponseEntity.ok(departments);
   }
 
   @PutMapping("/update")
   public ResponseEntity<DepartmentResponseDTO> updateDepartment(
       @Valid @RequestBody UpdatedDepartmentDTO department) {
-    final Department updatedDepartment = updateDepartmentService.execute(department);
-    return ResponseEntity.ok(new DepartmentResponseDTO(updatedDepartment));
+    return ResponseEntity.ok(updateDepartmentService.execute(department));
   }
 
   @PutMapping("promote/manager")
   public ResponseEntity<DepartmentResponseDTO> promoteManager(
       @Valid @RequestBody PromoteManagerDTO dto) {
-    final Department updatedDepartment = promoteManagerService.execute(dto);
-    return ResponseEntity.ok(new DepartmentResponseDTO(updatedDepartment));
+    return ResponseEntity.ok(promoteManagerService.execute(dto));
   }
 
   @DeleteMapping("/delete/{id}")
