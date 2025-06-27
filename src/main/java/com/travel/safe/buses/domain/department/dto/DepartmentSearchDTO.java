30f85c37139c23a9f25e97d0a9c5962cd5aac571
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-27 04:34:29 +0300

    fix(dto): fix defualt pagealbe dto

--- src/main/java/com/travel/safe/buses/domain/department/dto/DepartmentSearchDTO.java
+++ src/main/java/com/travel/safe/buses/domain/department/dto/DepartmentSearchDTO.java
@@ -1,10 +1,11 @@
 package com.travel.safe.buses.domain.department.dto;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public record DepartmentSearchDTO(@RequestParam(required = false) String departmentName,
-                                  @PageableDefault Pageable pageable) {
+                                  @PageableDefault(page = 0, size = 20, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
 
 }
