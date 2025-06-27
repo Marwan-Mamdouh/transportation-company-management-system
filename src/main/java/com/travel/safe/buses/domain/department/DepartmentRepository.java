22418a6cd456d6a255f358e832d0213d554020d6
marwan mamdouh <marwanabdalmagied@gmail.com>
2025-06-27 04:32:58 +0300

    feat:add findName method with pageable parameter

--- src/main/java/com/travel/safe/buses/domain/department/DepartmentRepository.java
+++ src/main/java/com/travel/safe/buses/domain/department/DepartmentRepository.java
@@ -1,6 +1,8 @@
 package com.travel.safe.buses.domain.department;
 
 import com.travel.safe.buses.domain.department.model.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface DepartmentRepository extends JpaRepository<Department, Integer>,
     JpaSpecificationExecutor<Department> {
 
+  Page<Department> findByName(String name, Pageable pageable);
 }
